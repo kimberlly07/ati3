b55322d1c351c578b487c48839e28dd8bec9c26e
kimberlly07 <142352722+kimberlly07@users.noreply.github.com>
2024-10-11 11:59:56 -0300

    fhm

---
+++ perguntas.js
@@ -0,0 +1,100 @@
+criaCartao(
+    'Matemática',
+    'Qual a raiz quadrada de 169',
+    '13')
+    
+    criaCartao(
+    'Matemática',
+    'Qual é o valor de 2³?',
+    '8')
+    
+    criaCartao(
+    'Matemática',
+    'Quanto é 7x8?',
+    '56')
+    
+    criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 49?',
+    '7')
+    
+    criaCartao(
+    'Matemática',
+    'Quanto é 12÷4?',
+    '3')
+    
+    criaCartao(
+    'Matemática',
+    'Qual é o resultado de 5²',
+    '25')
+    
+    criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em área?',
+    'Rússia')
+    
+    criaCartao(
+    'Geografia',
+    'Qual é o país mais populoso do mundo?',
+    'China')
+    
+    criaCartao(
+    'Geografia',
+    'Qual o maior rio do mundo?',
+    'Rio Nilo')
+    
+    criaCartao(
+    'Língua portuguesa',
+    'Complete corretamente a frase: "Ela foi ___ mercado.',
+    'ao')
+    
+    criaCartao(
+    'Língua portuguesa',
+    'Qual é o antônimo de "claro"?',
+    'escuro')
+    
+    criaCartao(
+    'Língua Portuguesa',
+    'Qual é a classe gramatical da palavra "rápido" na frase "O carro é rápido"?',
+    'Adjetivo')
+    
+    criaCartao(
+    'Língua Portuguesa',
+    'Qual é o tempo verbal da frase "Eu fui ao cinema"?',
+    'Passado')
+    
+    criaCartao(
+    'Língua Portuguesa',
+    'O que é um sujeito oculto?',
+    'Um sujeito que não está explícito na oração')
+    
+    criaCartao(
+    'História',
+    'Qual país colonizou o Brasil?',
+    'Portugual')
+    
+    criaCartao(
+    'Língua Inglesa',
+    'Qual é o passado de go?',
+    'Fui')
+    
+    criaCartao(
+    'Língua Inglesa',
+    'Como se diz "cachorro" em inglês?',
+    'Dog')
+    
+    criaCartao(
+    'Língua Inglesa',
+    'Qual é o plural de criança?',
+    'Children')
+    
+    criaCartao(
+    'Língua Inglesa',
+    'Qual é o oposto de quente em inglês?',
+    'Cold')
+    
+    criaCartao(
+    'Língua Inglesa',
+    'Qual é a tradução da frase "I like music" em português?',
+    'Eu gosto de música')
+    
