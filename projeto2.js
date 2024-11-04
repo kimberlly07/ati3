5a8564c9d2ab7947271cf8bcd9a621628004f9a5
kimberlly07 <142352722+kimberlly07@users.noreply.github.com>
2024-11-04 08:25:32 -0300

    dg

--- projeto2.js
+++ projeto2.js
@@ -16,3 +16,13 @@ function criaCartao(categoria,pergunta,resposta){
     
     container.appendChild(cartao);
 }
+
+function mostrarRespoosta (card){
+    const resposta = card.querySelector('.resposta-cartao')
+    if(resposta.style.display --- 'none'){
+        resposta.style.display = 'block'
+    }
+    else{ resposta.style.display = 'none'
+    }
+
+}
